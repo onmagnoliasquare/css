07a533076dd7e4d262f0af0d399cc834101eb1e9
n30w <hello@neoalabastro.com>
2024-04-03 00:43:39 +0800

    build: add --preserve-variables to build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Workaround to 'yarn build', since it somehow truncates
 # the CSS output and does not complete the build process
 # which results in an incomplete CSS file.
-yarn tachyons src/tachyons.css > css/tachyons.css && yarn tachyons src/tachyons.css --minify > css/tachyons.min.css
+yarn tachyons src/tachyons.css --preserve-variables > css/tachyons.css && yarn tachyons src/tachyons.css --preserve-variables --minify > css/tachyons.min.css
